09495770ff5356c8d52b139942d17daa66f3b696
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-04-23 13:48:52 +0530

    les goo

--- kamingo.js
+++ kamingo.js
@@ -1,6 +1,8 @@
 
 var express = require('express')
 var app = express()
+const { auth, requiresAuth } = require("express-openid-connect");
+require("dotenv").config();
 
 var con = require("./database.js");
 
@@ -11,21 +13,91 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  issuerBaseURL: process.env.ISSUER,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  secret: process.env.SECRET,
+  idpLogout: true,
+};
+
+app.use(auth(config));
+
+app.use((req, res, next) => {
+  user = JSON.stringify(req.oidc.user["sid"], null, 2).replace(/"/g, "")
+  // console.log(req.baseUrl);
+  // user  = req.baseUrl.replace("/dashboard/" , "")
+  console.log(user);
+  next();
+});
+
+
+
 app.get('/', function (req, res) {
-  res.render('home')
+  
+  con.query(
+    `SELECT * FROM profiles`,
+    
+    function (err, result, fields) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(result);
+
+    
+      res.render('home' , {isAuthenticated : req.oidc.isAuthenticated(),data: result} )
+    }
+  );
+
 })
 app.get('/Create', function (req, res) {
   res.render('createpost')
 })
-app.get('/EditProfile', function (req, res) {
-  res.render('profiles/editprofile')
-})
-app.post('/EditProfile', function (req, res) {
-  const file = req.files.image;
-  const {name, contact , address , description , shopname } = req.body
+
+
+
+app.get('/editprofile', function (req, res) {
+
+  user = JSON.stringify(req.oidc.user["sid"], null, 2).replace(/"/g, "")
+
+
+  con.query(
+    `SELECT id ,name, image, service , contact , address , description , shopname  FROM profiles`,
   
-  file.mv('uploads/profiles/' + file.name, (err) => {
+    function (err, result, fields) {
+      if (err) {
+        console.log(err);
+      }
+      result.forEach(element => {
+        if (result[0]["id"] === user) {
+          console.log(element);
+          res.render('profiles/editprofile' , {isAuthenticated : req.oidc.isAuthenticated() , data:element} )
+        }
+        else{
+          res.render('profiles/createprofile' , {isAuthenticated : req.oidc.isAuthenticated()})
+        }
+        console.log(result[0]["id"]);
+      });
+    }
+  );
+
+
+})
+
+
+
+app.post('/createprofile', function (req, res) {
+  const file = req.files.image;
+  user = JSON.stringify(req.oidc.user["sid"], null, 2).replace(/"/g, "")
+  const {name, service ,  contact , address , description , shopname } = req.body
+  console.log(req.body);
+  
+  file.mv('public/uploads/profiles/' + file.name, (err) => {
     if (err) {
+      console.log(err);
       return res.status(500).send(err);
     }
     })
@@ -33,7 +105,7 @@ app.post('/EditProfile', function (req, res) {
     
 
     con.query(
-      `INSERT INTO profiles (name, contact , address , description , shopname) VALUES ('${name}' , '${contact}','${address}','${description}','${shopname}');`,
+      `INSERT INTO profiles ( id ,name, image, service , contact , address , description , shopname) VALUES ('${user}','${name}' ,'${file.name}', '${service}' , '${contact}','${address}','${description}','${shopname}');`,
     
       function (err, result, fields) {
         if (err) {
@@ -44,9 +116,13 @@ app.post('/EditProfile', function (req, res) {
     );
 
 
-    res.send('File uploaded successfully!');
+    res.redirect("/")
   
 })
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 app.listen(3030)
 
