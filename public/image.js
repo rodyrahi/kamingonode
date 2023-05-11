0224aa69c845787d0137220d3d1f29790d2e6260
rodyrahi <rodyrahi126@gmail.com>
2023-05-11 10:56:04 +0530

    image height fixed

--- public/image.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-const myDiv = document.querySelectorAll('.image-box');
-myDiv.forEach(element => {
-   w= element.clientWidth;
-  element.style.height = w.toString() + "px";
-   console.log(w , element.clientHeight);
-});
-}
