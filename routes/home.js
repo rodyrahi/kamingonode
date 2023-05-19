eaa637dac67e05d0a07b96057beaf181f464f6e0
rodyrahi <rodyrahi126@gmail.com>
2023-05-19 18:05:36 +0530

    bb

--- routes/home.js
+++ routes/home.js
@@ -3,199 +3,130 @@ const app = express();
 var router = express.Router();
 var con = require("../database.js");
 
+function executeQuery(query) {
+  return new Promise((resolve, reject) => {
+    con.query(query, (err, result, fields) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
 
 
-router.get("/", function (req, res) {
+router.get("/", async (req, res) => {
     if (req.oidc.isAuthenticated()) {
-      con.query(
-        `SELECT * FROM profiles`,
-  
-        function (err, result, fields) {
-          if (err) {
-            console.log(err);
-          }
-          console.log(result.name);
-          con.query(
-            `SELECT * FROM comments `,
-            function (err, comments, fields) {
-              if (err) {
-                console.log(err);
-              }
 
-              // console.log('here');
+      const result = await executeQuery(`SELECT * FROM profiles`)
+      const comments = await executeQuery(`SELECT * FROM profiles`)
+      
+      res.render("home", {
+        isAuthenticated: req.oidc.isAuthenticated(),
+        data: result,
+        rating: comments,
+       })
 
-              let rating = 0;
-
-              // comments.forEach((element) => {
-              //   rating += element["rating"];
-              // });
-
-              // console.log(rating);
-              // totalrating = rating / comments.length;
-
-              // console.log(totalrating);
-              res.render("home", {
-                isAuthenticated: req.oidc.isAuthenticated(),
-                data: result,
-                rating: comments
-              });
-
-              
-            }
-          );
-       
-  
-          
-        }
-      );
     } else {
       res.redirect("/login");
     }
   });
 
 
-router.post("/filter", function (req, res) {
+router.post("/filter", async (req, res) => {
     const{search , city , area , service , highest } = req.body
     filters = [search , city , area , service , highest]
     console.log(req.body);
     if (req.oidc.isAuthenticated()) {
         
-        
-            
-        
-      con.query(
-        `SELECT * FROM profiles`,
-  
-        function (err, result, fields) {
-          if (err) {
-            console.log(err);
-          }
-        //   if (city && area && highest && search && service) {
-        //     console.log('yes');
-        //     result = result.filter(item => item.city === city && item.area === area && item.highest === highest && item.key.includes(search) && item.service === service );
-        //     console.log(result);
-        //   }
-        //   else if (city && area && highest && search){
+      let result = await executeQuery(`SELECT * FROM profiles`)
 
-        //   }
-
-        filters.forEach(element => {
-            if (element && element !== search) {
-              
-            result = result.filter(item  => 
-              // console.log(element + "this");
-              item.city === element || item.area === element || item.service === element
-             );
-            // console.log();
-            }
-           if (element && element === search) {
-                console.log(element);
-                result = result.filter(item => item.name.toLowerCase().includes(element.toLowerCase()) || item.service.toLowerCase().includes(element.toLowerCase()) || item.city.toLowerCase().includes(element.toLowerCase()) || item.area.toLowerCase().includes(element.toLowerCase())  );
-                }
-            
-        });
-
-          res.render("home", {isAuthenticated: req.oidc.isAuthenticated(),data: result,});
+      if (result) {
+      await Promise.all(filters.map(async (element) => {
+        if (element && element !== search && element !== highest ) {
+          result = result.filter(
+            (item) =>
+              item.city === element ||
+              item.area === element ||
+              item.service === element
+          );
         }
-      );
-        
+        if (element && element === search) {
+          console.log(element);
+         result= result.filter(
+            (item) =>
+              item.name.toLowerCase().includes(element.toLowerCase()) ||
+              item.service.toLowerCase().includes(element.toLowerCase()) ||
+              item.city.toLowerCase().includes(element.toLowerCase()) ||
+              item.area.toLowerCase().includes(element.toLowerCase())
+          );
+        }
+        if (element && element === highest) {
+          const newresult = await executeQuery(`SELECT * FROM profiles ORDER BY rating DESC`)
+
+          result= newresult
+        }
+
+
+      
+        }))
+        res.render("home", {
+          isAuthenticated: req.oidc.isAuthenticated(),
+          data: result,
+        });
+      }
+
     } else {
       res.redirect("/login");
     }
   });
 
-
-
-
-router.get("/services/:name", function (req, res) {
+router.get("/services/:name", async (req, res) => {
   user = JSON.stringify(req.oidc.user["sub"], null, 2).replace(/"/g, "");
 
     if (req.oidc.isAuthenticated()) {
-      con.query(
-        `SELECT * FROM profiles  WHERE name ="${req.params.name}"`,
-  
-        function (err, result, fields) {
-          if (err) {
-            console.log(err);
-          }
-          console.log(result[0]);
-  
-          con.query(
-            `SELECT * FROM skill  WHERE id ="${result[0]["id"]}"`,
-            function (err, skills, fields) {
-              if (err) {
-                console.log(err);
-              }
-              con.query(
-                `SELECT * FROM comments  WHERE post='${req.params.name}'`,
-                function (err, comments, fields) {
-                  if (err) {
-                    console.log(err);
-                  }
-  
-                  // console.log('here');
-  
-                  let rating = 0;
-                  let userrating = 0;
-
-  
-                  comments.forEach((element) => {
-                    rating += element["rating"];
-                    if (element.id === user) {
-                      userrating = element["rating"];
-
-                    }
-                  });
-  
-                  console.log(rating);
-                  totalrating = rating / comments.length;
-                  totalrating = Math.round(totalrating * 10)/10
-                  con.query(
-                    `UPDATE profiles SET rating='${totalrating}' WHERE name ="${req.params.name}"`,
-                    function (err, comments, fields) {
-                      if (err) {
-                        console.log(err);
-                      }
-                    
-                  });
-  
-                  console.log(totalrating);
-  
-                  res.render("profiles/profiledetail", {
-                    isAuthenticated: req.oidc.isAuthenticated(),
-                    data: result[0],
-                    comments: comments,
-                    user: req.oidc.user["sub"],
-                    skills: skills,
-                    rating: totalrating,
-                    userrating: userrating,
-                    ratingno:comments.length
-                  });
-                }
-              );
-            }
-          );
-  
-          // con.query(
-          //   `SELECT * FROM comments  WHERE id ="${result[0]["id"]}"`,
-  
-          //   function (err, comments, fields) {
-          //     if (err) {
-          //       console.log(err);
-          //     }
-          //     con.query(
-          //       `SELECT * FROM skill  WHERE id ="${result[0]["id"]}"`,
-  
-          //       function (err, skills, fields) {
-          //         if (err) {
-          //           console.log(err);
-          //         }
-          //         res.render('profiles/profiledetail' , {isAuthenticated : req.oidc.isAuthenticated(),data: result[0] , comments: comments , user:req.oidc.user["sub"],skills:skills} )
-  
-          //       });
-          //   });
-        }
+      const result = await executeQuery(
+        `SELECT * FROM profiles  WHERE name ="${req.params.name}"`
       );
+      const skills = await executeQuery(
+        `SELECT * FROM skill  WHERE id ="${result[0]["id"]}"`
+      );
+      const comments = await executeQuery(
+        `SELECT * FROM comments  WHERE post='${req.params.name}'`
+      );
+
+      let rating = 0;
+      let userrating = 0;
+      if (comments) {
+        comments.forEach((element) => {
+          rating += element["rating"];
+          if (element.id === user) {
+            userrating = element["rating"];
+          }
+        });
+
+        console.log(rating);
+        totalrating = rating / comments.length;
+        totalrating = Math.round(totalrating * 10) / 10;
+
+        await executeQuery(
+          `UPDATE profiles SET rating='${totalrating}' WHERE name ="${req.params.name}"`
+        );
+
+        console.log(totalrating);
+
+        res.render("profiles/profiledetail", {
+          isAuthenticated: req.oidc.isAuthenticated(),
+          data: result[0],
+          comments: comments,
+          user: req.oidc.user["sub"],
+          skills: skills,
+          rating: totalrating,
+          userrating: userrating,
+          ratingno: comments.length,
+        });
+      }
     } else {
       res.redirect("/login");
     }
--- routes/profile.js
+++ routes/profile.js
@@ -4,351 +4,287 @@ var router = express.Router();
 var con = require("../database.js");
 
 
-
-
-
-router.get("/editprofile", function (req, res) {
-    user = JSON.stringify(req.oidc.user["sub"], null, 2).replace(/"/g, "");
-  
-    con.query(
-      `SELECT *  FROM profiles WHERE id='${user}'`,
-  
-      function (err, result, fields) {
-        if (err) {
-        }
-  
-        console.log(result);
-        try {
-          con.query(
-            `SELECT * FROM skill WHERE id = '${result[0]["id"]}'`,
-  
-            function (err, skills, fields) {
-              if (err) {
-                console.log(err);
-              }
-              con.query(
-                `SELECT subcategory FROM service WHERE category = '${result[0]["service"]}'`,
-  
-                function (err, services, fields) {
-                  if (err) {
-                    console.log(err);
-                  }
-                  console.log(services);
-  
-                  res.render("profiles/editprofile", {
-                    isAuthenticated: req.oidc.isAuthenticated(),
-                    data: result[0],
-                    skills: skills,
-                    services: services,
-                  });
-                }
-              );
-            }
-          );
-        } catch (error) {
-          res.render("profiles/createprofile", {
-            isAuthenticated: req.oidc.isAuthenticated(),
-          });
-        }
+function executeQuery(query) {
+  return new Promise((resolve, reject) => {
+    con.query(query, (err, result, fields) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
       }
-    );
+    });
   });
+}
+
+
+router.get("/editprofile", async (req, res) => {
+  user = JSON.stringify(req.oidc.user["sub"], null, 2).replace(/"/g, "");
+
+  const result = await executeQuery(
+    `SELECT *  FROM profiles WHERE id='${user}'`
+  );
+  const skills = await executeQuery(
+    `SELECT * FROM skill WHERE id = '${result[0]["id"]}'`
+  );
+  const services = await executeQuery(
+    `SELECT subcategory FROM service WHERE category = '${result[0]["service"]}'`
+  );
+
+  if (skills) {
+    res.render("profiles/editprofile", {
+      isAuthenticated: req.oidc.isAuthenticated(),
+      data: result[0],
+      skills: skills,
+      services: services,
+    });
+  } else {
+    res.render("profiles/createprofile", {
+      isAuthenticated: req.oidc.isAuthenticated(),
+    });
+  }
+});
   
 router.post("/editprofile", async (req, res) => {
-    console.log(" post  ");
-    file = null;
-    try {
-      file = req.files.image;
-  
-      const filePath = file.path;
-  
-      console.log(file);
-    } catch (error) {}
-  
-    user = JSON.stringify(req.oidc.user["sub"], null, 2).replace(/"/g, "");
-    const {
-      name,
-      image,
-      service,
-      contact,
-      address,
-      description,
-      shopname,
-      price,
-      skill,
-      pincode,
-      area,
-      city,
-    } = req.body;
-    console.log(req.body);
-  
-    if (file) {
-      const sharp = require("sharp");
-  
-      file.mv("public/uploads/profiles/" + "raw-" + file.name, (err) => {
-        if (err) {
-          console.log(err);
-          return res.status(500).send(err);
-        }
-        const outputFilePath = "public/uploads/profiles/" + file.name;
-  
-        sharp("public/uploads/profiles/" + "raw-" + file.name)
-          .png({ quality: 60 })
-          .toFile(outputFilePath)
-          .then(() => {
-            console.log("Image compressed successfully!");
-          })
-          .then(() => {
-            fs.unlink("public/uploads/profiles/" + "raw-" + file.name, (err) => {
-              if (err) console.log(err);
-  
-              console.log("\nDeleted file: example_file.txt");
-            });
-          })
-          .catch((err) => {
-            console.error(err);
-            // remove the original file from the server (in case of an error)
+  console.log(" post  ");
+  file = null;
+  try {
+    file = req.files.image;
+    const filePath = file.path;
+    console.log(file);
+  } catch (error) {}
+
+  user = JSON.stringify(req.oidc.user["sub"], null, 2).replace(/"/g, "");
+  const {
+    name,
+    image,
+    service,
+    contact,
+    address,
+    description,
+    shopname,
+    price,
+    skill,
+    pincode,
+    area,
+    city,
+  } = req.body;
+  console.log(req.body);
+
+  if (file) {
+    const sharp = require("sharp");
+
+    file.mv("public/uploads/profiles/" + "raw-" + file.name, (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(err);
+      }
+      const outputFilePath = "public/uploads/profiles/" + file.name;
+
+      sharp("public/uploads/profiles/" + "raw-" + file.name)
+        .png({ quality: 60 })
+        .toFile(outputFilePath)
+        .then(() => {
+          console.log("Image compressed successfully!");
+        })
+        .then(() => {
+          fs.unlink("public/uploads/profiles/" + "raw-" + file.name, (err) => {
+            if (err) console.log(err);
+
+            console.log("\nDeleted file: example_file.txt");
           });
-      });
-  
-      const fs = require("fs");
-  
-      // assuming that the uploaded file is saved as 'file' in your code
-  
-      // console.log(filePath);
-  
-      con.query(
-        `UPDATE profiles SET name = '${name}', image='${file.name}', service='${service}' , contact='${contact}' , address='${address}' , description='${description}' , shopname='${shopname}' , pincode=${pincode}  , city='${city}' , area='${area}' where id='${user}'`,
-  
-        function (err, result, fields) {
-          if (err) {
-            console.log(err);
-          }
-          console.log(result);
-        }
-      );
-    } else {
-      con.query(
-        `UPDATE profiles SET name = '${name}', image='${image}', service='${service}' , contact='${contact}' , address='${address}' , description='${description}' , shopname='${shopname}', pincode=${pincode}  , city='${city}' , area='${area}' where id='${user}'`,
-  
-        function (err, result, fields) {
-          if (err) {
-            console.log(err);
-          }
-          console.log(result);
-        }
-      );
-    }
-  
-    con.query(
-      `DELETE FROM skill WHERE id = "${user}";`,
-      function (err, result, fields) {
-        if (err) {
-          console.log(err);
-        }
-        console.log(result);
-      }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+
+    const fs = require("fs");
+
+    await executeQuery(
+      `UPDATE profiles SET name = '${name}', image='${file.name}', service='${service}' , contact='${contact}' , address='${address}' , description='${description}' , shopname='${shopname}' , pincode=${pincode}  , city='${city}' , area='${area}' where id='${user}'`
     );
-  
-    console.log();
-  
-    if (Array.isArray(skill)) {
-      for (let index = 0; index < skill.length; index++) {
-        // const element = array[index];
-        con.query(
-          `INSERT INTO skill ( id ,service , price , post) VALUES ('${user}','${skill[index]}' ,'${price[index]}' ,'${name}')`,
-          function (err, result, fields) {
-            if (err) {
-              console.log(err);
-            }
-          }
-        );
-      }
-    } else {
-      con.query(
-        `INSERT INTO skill ( id ,service , price , post) VALUES ('${user}','${skill}' ,'${price}','${name}')`,
-        function (err, result, fields) {
-          if (err) {
-            console.log(err);
-          }
-        }
+  } else {
+    await executeQuery(
+      `UPDATE profiles SET name = '${name}', image='${image}', service='${service}' , contact='${contact}' , address='${address}' , description='${description}' , shopname='${shopname}', pincode=${pincode}  , city='${city}' , area='${area}' where id='${user}'`
+    );
+  }
+  await executeQuery(`DELETE FROM skill WHERE id = "${user}";`);
+
+  console.log();
+
+  if (Array.isArray(skill)) {
+    for (let index = 0; index < skill.length; index++) {
+      // const element = array[index];
+
+      await executeQuery(
+        `INSERT INTO skill ( id ,service , price , post) VALUES ('${user}','${skill[index]}' ,'${price[index]}' ,'${name}')`
       );
     }
-  
-    res.redirect("/");
-  });
+  } else {
+    await executeQuery(
+      `INSERT INTO skill ( id ,service , price , post) VALUES ('${user}','${skill}' ,'${price}','${name}')`
+    );
+  }
+
+  res.redirect("/");
+});
   
 router.post("/createprofile", async (req, res) => {
-    console.log(" post  ");
-    file = null;
-    try {
-      file = req.files.image;
-  
-      const filePath = file.path;
-  
-      console.log(file);
-    } catch (error) {}
-  
-    user = JSON.stringify(req.oidc.user["sub"], null, 2).replace(/"/g, "");
-    const {
-      name,
-      image,
-      service,
-      contact,
-      address,
-      description,
-      shopname,
-      skill,
-      price,
-      pincode,
-      area,
-      city,
-    } = req.body;
-    console.log(req.body);
-  
-    if (file) {
-      const sharp = require("sharp");
-  
-      file.mv("public/uploads/profiles/" + "raw-" + file.name, (err) => {
-        if (err) {
-          console.log(err);
-          return res.status(500).send(err);
-        }
-        const outputFilePath = "public/uploads/profiles/" + file.name;
-  
-        sharp("public/uploads/profiles/" + "raw-" + file.name)
-          .png({ quality: 50 })
-          .toFile(outputFilePath)
-          .then(() => {
-            console.log("Image compressed successfully!");
-          })
-          .then(() => {
-            fs.unlink("public/uploads/profiles/" + "raw-" + file.name, (err) => {
-              if (err) console.log(err);
-  
-              console.log("\nDeleted file: example_file.txt");
-            });
-          })
-          .catch((err) => {
-            console.error(err);
-            // remove the original file from the server (in case of an error)
-          });
-      });
-  
-      const fs = require("fs");
-  
-      // assuming that the uploaded file is saved as 'file' in your code
-  
-      // console.log(filePath);
-  
-      con.query(
-        `INSERT INTO profiles ( id ,name, image, service , contact , address , description , shopname,pincode,area,city) VALUES 
-        ('${user}','${name}' ,'${file.name}', '${service}' , '${contact}','${address}','${description}','${shopname}','${pincode}','${area}' ,'${city}');`,
-  
-        function (err, result, fields) {
-          if (err) {
-            console.log(err);
-          }
-          console.log(result);
-        }
-      );
-    } else {
-      con.query(
-        `INSERT INTO profiles ( id ,name, image, service , contact , address , description , shopname ,pincode,area,city) VALUES 
-        ('${user}','${name}' ,'${"kamingo_favicon.png"}', '${service}' , '${contact}','${address}','${description}','${shopname}','${pincode}','${area}' ,'${city}');`,
-  
-        function (err, result, fields) {
-          if (err) {
-            console.log(err);
-          }
-          console.log(result);
-        }
-      );
-    }
-  
-    // con.query(
-    //   `DELETE FROM skill WHERE id = "${user}";`,
-    //   function (err, result, fields) {
-    //     if (err) {
-    //       console.log(err);
-    //     }
-    //     console.log(result);
-    //   }
-    // );
-  
-    console.log();
-  
-    if (Array.isArray(skill)) {
-      for (let index = 0; index < skill.length; index++) {
-        // const element = array[index];
-        con.query(
-          `INSERT INTO skill ( id ,service , price) VALUES ('${user}','${skill[index]}' ,'${price[index]}' ,'${name}' )`,
-          function (err, result, fields) {
-            if (err) {
-              console.log(err);
-            }
-          }
-        );
+  console.log(" post  ");
+  file = null;
+  try {
+    file = req.files.image;
+
+    const filePath = file.path;
+
+    console.log(file);
+  } catch (error) {}
+
+  user = JSON.stringify(req.oidc.user["sub"], null, 2).replace(/"/g, "");
+  const {
+    name,
+    image,
+    service,
+    contact,
+    address,
+    description,
+    shopname,
+    skill,
+    price,
+    pincode,
+    area,
+    city,
+  } = req.body;
+  console.log(req.body);
+
+  if (file) {
+    const sharp = require("sharp");
+
+    file.mv("public/uploads/profiles/" + "raw-" + file.name, (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(err);
       }
-    } else {
-      con.query(
-        `INSERT INTO skill ( id ,service , price , post) VALUES ('${user}','${skill}' ,'${price}','${name}')`,
-        function (err, result, fields) {
-          if (err) {
-            console.log(err);
-          }
-        }
-      );
-    }
-  
-    res.redirect("/");
-  });
-  
-  router.get("/createprofile", function (req, res) {
-    user = JSON.stringify(req.oidc.user["sub"], null, 2).replace(/"/g, "");
-  
+      const outputFilePath = "public/uploads/profiles/" + file.name;
+
+      sharp("public/uploads/profiles/" + "raw-" + file.name)
+        .png({ quality: 50 })
+        .toFile(outputFilePath)
+        .then(() => {
+          console.log("Image compressed successfully!");
+        })
+        .then(() => {
+          fs.unlink("public/uploads/profiles/" + "raw-" + file.name, (err) => {
+            if (err) console.log(err);
+
+            console.log("\nDeleted file: example_file.txt");
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+
+    const fs = require("fs");
+
     con.query(
-      `SELECT *  FROM profiles WHERE id='${user}'`,
-  
+      `INSERT INTO profiles ( id ,name, image, service , contact , address , description , shopname,pincode,area,city) VALUES 
+        ('${user}','${name}' ,'${file.name}', '${service}' , '${contact}','${address}','${description}','${shopname}','${pincode}','${area}' ,'${city}');`,
+
       function (err, result, fields) {
         if (err) {
+          console.log(err);
         }
-  
         console.log(result);
-        try {
-          con.query(
-            `SELECT * FROM skill WHERE id = '${result[0]["id"]}'`,
-  
-            function (err, skills, fields) {
-              if (err) {
-                console.log(err);
-              }
-              con.query(
-                `SELECT subcategory FROM service WHERE category = '${result[0]["service"]}'`,
-  
-                function (err, services, fields) {
-                  if (err) {
-                    console.log(err);
-                  }
-                  console.log(services);
-  
-                  res.render("profiles/editprofile", {
-                    isAuthenticated: req.oidc.isAuthenticated(),
-                    data: result[0],
-                    skills: skills,
-                    services: services,
-                  });
-                }
-              );
-            }
-          );
-        } catch (error) {
-          res.render("profiles/createprofile", {
-            isAuthenticated: req.oidc.isAuthenticated(),
-          });
+      }
+    );
+  } else {
+    con.query(
+      `INSERT INTO profiles ( id ,name, image, service , contact , address , description , shopname ,pincode,area,city) VALUES 
+        ('${user}','${name}' ,'${"kamingo_favicon.png"}', '${service}' , '${contact}','${address}','${description}','${shopname}','${pincode}','${area}' ,'${city}');`,
+
+      function (err, result, fields) {
+        if (err) {
+          console.log(err);
+        }
+        console.log(result);
+      }
+    );
+  }
+
+  console.log();
+
+  if (Array.isArray(skill)) {
+    for (let index = 0; index < skill.length; index++) {
+      // const element = array[index];
+      con.query(
+        `INSERT INTO skill ( id ,service , price) VALUES ('${user}','${skill[index]}' ,'${price[index]}' ,'${name}' )`,
+        function (err, result, fields) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+    }
+  } else {
+    con.query(
+      `INSERT INTO skill ( id ,service , price , post) VALUES ('${user}','${skill}' ,'${price}','${name}')`,
+      function (err, result, fields) {
+        if (err) {
+          console.log(err);
         }
       }
     );
-  });
+  }
+
+  res.redirect("/");
+});
+
+router.get("/createprofile", async (req, res) => {
+  user = JSON.stringify(req.oidc.user["sub"], null, 2).replace(/"/g, "");
+
+  con.query(
+    `SELECT *  FROM profiles WHERE id='${user}'`,
+
+    function (err, result, fields) {
+      if (err) {
+      }
+
+      console.log(result);
+      try {
+        con.query(
+          `SELECT * FROM skill WHERE id = '${result[0]["id"]}'`,
+
+          function (err, skills, fields) {
+            if (err) {
+              console.log(err);
+            }
+            con.query(
+              `SELECT subcategory FROM service WHERE category = '${result[0]["service"]}'`,
+
+              function (err, services, fields) {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(services);
+
+                res.render("profiles/editprofile", {
+                  isAuthenticated: req.oidc.isAuthenticated(),
+                  data: result[0],
+                  skills: skills,
+                  services: services,
+                });
+              }
+            );
+          }
+        );
+      } catch (error) {
+        res.render("profiles/createprofile", {
+          isAuthenticated: req.oidc.isAuthenticated(),
+        });
+      }
+    }
+  );
+});
 
 module.exports = router
